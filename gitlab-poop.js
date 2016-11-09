eb30bac420ec3c02f88bca3b9a2a87bfc304f3c3
Marco Fugaro <marco.fugaro@gmail.com>
2016-11-09 10:01:58 +0100

    added support also for about.gitlab.com

--- gitlab-poop.js
+++ gitlab-poop.js
@@ -1,10 +1,22 @@
 const poopURL = chrome.extension.getURL('poop-emoji.png')
 
 function updateLogos() {
-  const logo = document.getElementById('logo')
-  if (logo) logo.innerHTML = `<img src="${poopURL}">`
+  // let's update the main logos
+  document.querySelectorAll('#logo, .center-logo, .blank-header').forEach(el => el.innerHTML = `<img src="${poopURL}">`)
 
-  document.querySelectorAll('[alt="GitLab Logo"]').forEach(el => el.src = poopURL)
+  // let's update the svg logos
+  document.querySelectorAll('.nav-logo, .footer-logo').forEach((el) => {
+    const parent = el.parentNode
+
+    parent.insertAdjacentHTML('afterBegin', `<img src="${poopURL}" class="${el.className.baseVal}">`)
+    parent.removeChild(el)
+  })
+
+  // image in the footer in homepage fix
+  document.styleSheets[0].insertRule('.footer-logo { width: 64px; }', 1)
+
+  // let's update the error pages' logos
+  document.querySelectorAll('[alt*="Logo"]').forEach(el => el.src = poopURL)
 }
 
 function updateFavicon() {
